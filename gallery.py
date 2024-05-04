875dddd043af7c65c3569fff5041e1343b7c56b7
H. Ryott Glayzer <ryott@ryott.gay>
2024-05-03 20:08:37 -0600

    Python Script CronJob

--- gallery.py
+++ gallery.py
@@ -1,17 +1,16 @@
 # This file is made to create gallery files
-import glob
+# Copyright H. Ryott Glayzer 2024
+# MIT License
 import os
-import sys
+import random
 
 
 dir = "/home/ryott/Projects/gutter-shows-website/gallery"
-f = open("pix.txt", "a")
-extlist = [".jpg", ".JPG", ".jpeg", ".gif"]
+f = open("pix.html", "w")
 pyx = [k for k in os.listdir(dir) if os.path.isfile(os.path.join(dir, k))]
-for file in pyx:
-    for ext in extlist:
-        if file.endswith(ext):
-            print(str(file))
-            f.write(f'<img src="gallery/{file}" height="200px">\n')
+newGallery = random.sample(pyx, 20)            
+for file in newGallery:
+    print(str(file))
+    f.write(f'<img src="gallery/{file}" height="200px">\n')
 
 f.close()
