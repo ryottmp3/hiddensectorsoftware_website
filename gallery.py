db4aeccfb0405a10632bd538e28003c152dcb2a3
H. Ryott Glayzer <ryott@ryott.gay>
2024-05-03 20:13:49 -0600

    :Qj

--- gallery.py
+++ gallery.py
@@ -5,7 +5,7 @@ import os
 import random
 
 cwd = os.getcwd()
-dir = as.path.join(cwd, "gallery")
+dir = os.path.join(cwd, "gallery")
 f = open(os.path.join(cwd, "pix.html"), "w")
 pyx = [k for k in os.listdir(dir) if os.path.isfile(os.path.join(dir, k))]
 newGallery = random.sample(pyx, 20)            
